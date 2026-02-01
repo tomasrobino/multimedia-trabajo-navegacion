48ca85ba865af9ed39744c59669ea4c3216a0338
Tomas Robino <tomaserobino@gmail.com>
2026-02-01 15:28:48 +0100

    supressing unused padding error

--- app/src/main/java/com/example/multimediatrabajonavegacion/MainActivity.kt
+++ app/src/main/java/com/example/multimediatrabajonavegacion/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.multimediatrabajonavegacion
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,12 +11,13 @@ import androidx.compose.ui.Modifier
 import com.example.multimediatrabajonavegacion.ui.theme.MultimediaTrabajoNavegacionTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MultimediaTrabajoNavegacionTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize()) { _ ->
                     NavigationComponent()
                 }
             }
